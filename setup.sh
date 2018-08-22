fd7b383a286ce9ae1eb60fbe441a2eb0d63d031d
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2018-08-21 23:26:24 -0500

    move vim-mutt

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ mkdir -p ${HOME}/bin
 mkdir -p ${HOME}/.config/git
 mkdir -p ${HOME}/.config/shrc
 mkdir -p ${HOME}/.config/systemd/user
+mkdir -p ${HOME}/.mutt
 stow -t ${HOME}/.config config
 stow -t ${HOME} tmux
 stow -t ${HOME} dircolors
 stow -t ${HOME} bash
 stow -t ${HOME} bin
+stow -t ${HOME} mutt
