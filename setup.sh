45d39d45559caace441288b41e1ed1f8cc233ffe
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2018-08-21 23:28:58 -0500

    fix path

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ mkdir -p ${HOME}/.config/shrc
 mkdir -p ${HOME}/.config/systemd/user
 mkdir -p ${HOME}/.mutt
 stow -t ${HOME}/.config config
+stow -t ${HOME}/.mutt mutt
 stow -t ${HOME} tmux
 stow -t ${HOME} dircolors
 stow -t ${HOME} bash
 stow -t ${HOME} bin
-stow -t ${HOME} mutt
