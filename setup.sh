e34b06bf89ad82dd2873422c62ab6b76b1cfc1a0
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2019-02-19 12:54:18 -0600

    add fluxbox

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ mkdir -p ${HOME}/.config/git
 mkdir -p ${HOME}/.config/shrc
 mkdir -p ${HOME}/.config/systemd/user
 mkdir -p ${HOME}/.mutt
+mkdir -p ${HOME}/.fluxbox
 mkdir -p ${HOME}/.vim/colors
 stow -t ${HOME}/.config config
 stow -t ${HOME}/.mutt mutt
 stow -t ${HOME}/.vim vim
+stow -t ${HOME}/.fluxbox fluxbox
 stow -t ${HOME} tmux
 stow -t ${HOME} dircolors
 stow -t ${HOME} bash
