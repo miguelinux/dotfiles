ad09e0dd7bf9cc5467e52b1e142b70ead3af599b
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2019-12-10 10:46:10 -0600

    add teclado script

---
+++ bin/teclado.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+# Synergy Server
+SS=$2
+
+if [ "$1" != "off" ]
+then
+    SS=$1
+fi
+
+test -n "${SS}" || SS=mobl3
+
+if [ "$1" = "off" ]
+then
+    systemctl --user stop synergy-client-ssh.service
+    systemctl --user stop synergy-tunnel@${SS}
+    echo stopped
+    exit
+fi
+
+if nmcli -t device | grep --quiet OfficeW
+then
+    if ssh ${SS} echo > /dev/null
+    then
+        systemctl --user start synergy-tunnel@${SS}
+        sleep 1 
+        systemctl --user start synergy-client-ssh.service
+    fi
+fi
