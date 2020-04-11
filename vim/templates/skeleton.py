c9e08172d5a3f9b5931241163a35082c02ef53c9
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2020-04-11 13:38:11 -0500

    add Python vim template

---
+++ vim/templates/skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+"""
+Ejemplo de un modulo
+"""
+
+def main():
+    """
+    Comentario de la función
+    """
+    print("Hola Mundo")
+
+
+if __name__ == "__main__":
+    main()
