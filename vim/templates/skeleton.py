031cf9c02634de039667dfe85247b17f6e6c1b6f
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2020-04-12 16:44:00 -0500

    add SPDX license

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -2,6 +2,8 @@
 # c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
 # vi: set shiftwidth=4 tabstop=8 expandtab:
 # :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 """
 Ejemplo de un modulo
 """
