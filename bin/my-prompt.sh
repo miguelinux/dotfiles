da02e701e51f2860c9a3df0388ed33b2656608eb
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-02-04 17:29:55 -0600

    bin: improve my-promt

--- bin/my-prompt.sh
+++ bin/my-prompt.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/echo "Este script tiene que correrse con source o ."
+#!/usr/bin/bash
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 #
@@ -6,6 +6,9 @@
 
 u=$1
 h=$2
+
+# Path to prompt file
+PTPF=${HOME}/.config/shrc/zz-prompt
 
 if [ -z "$u" ]
 then
@@ -16,9 +19,12 @@ then
     h="\\h"
 fi
 
-##"PS1=[\u@\h \W]\$" # old
-# \[\e[38;5;39m\]\u\[\e[0m\]@\[\e[38;5;208m\]\H \[\e[38;5;39m\]\w \[\e[38;5;39m\]$ \[\e[0;0m\]
-PS_USER="\[\e[38;5;39m\]${u}\[\e[0m\]@\[\e[38;5;208m\]${h} \[\e[38;5;39m\]\w\[\e[0;0m\]"
-PS_USER_END=" \[\e[38;5;39m\]$ \[\e[0;0m\]"
-#export PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
-export PROMPT_COMMAND='__git_ps1 "$PS_USER" "$PS_USER_END"'
+if [ "$u" = "\\u" -a "$h" = "\\h" ]
+then
+    rm -f ${PTPF}
+    exit
+fi
+
+echo "PS_USER=\"\[\e[38;5;39m\]${u}\[\e[0m\]@\[\e[38;5;208m\]${h} \[\e[38;5;39m\]\w\[\e[0;0m\]\"" > ${PTPF}
+echo "PS_USER_END=\" \[\e[38;5;39m\]$ \[\e[0;0m\]\"" >> ${PTPF}
+echo "export PROMPT_COMMAND='__git_ps1 \"\$PS_USER\" \"\$PS_USER_END\"'" >> ${PTPF}
