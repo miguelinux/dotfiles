44aad18f1e25b6d5847443d0adfd67597185a823
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2023-07-07 23:26:50 -0600

    bin: cifra y decifra

---
+++ bin/cifra.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if [ "${0##*/}" = "decifra.sh" ]
+then
+    gpg --decrypt --quiet --batch --passphrase-file $1 $2
+else
+    gpg --symmetric --cipher-algo AES256 --batch --passphrase-file $1 $2
+fi
