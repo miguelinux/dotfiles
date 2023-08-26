5d6de3393e27693c2bee48e64e1f199893315dac
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2023-08-25 23:05:20 -0600

    add my-dotfiles and my-radio

---
+++ bin/my-dotfiles.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+function update()
+{
+    repo_path_full=$(realpath $0)
+    repo_path=${repo_path_full%/*/*}
+    git -C "$repo_path" pull
+    #if ! git -C "${repo_path}" diff-index --cached --quiet HEAD --
+    if [ -n "$(git -C \"$repo_path\" status --porcelain)" ]
+    then
+        git -C "${repo_path}" add --all
+        git -C "${repo_path}" commit -m "auto-update"
+        git -C "${repo_path}" push
+    fi
+    bash "${repo_path}"/setup.sh
+}
+
+function myhelp()
+{
+    echo "Usage: $0 <command>"
+    echo ""
+    echo "COMMANDS"
+    echo ""
+    echo "update         updates the repo"
+    echo ""
+    exit
+}
+
+if [ "$#" == "0" ]
+then
+    myhelp
+fi
+
+while [ -n "${1}" ]
+do
+    case "$1" in
+        -d|--debug)
+            set -x
+        ;;
+        -e|--error)
+            set -e
+        ;;
+        update)
+            update
+        ;;
+        -h|--help)
+            myhelp
+        ;;
+        *)
+            myhelp
+        ;;
+    esac
+    shift
+done
+
---
+++ bin/my-radio.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+estaciones="
+metropoli https://s2.yesstreaming.net:9088/stream
+"
+
+function toca() 
+{
+    found=0
+    for est in $estaciones
+    do
+        if [ "$found" = "1" ]
+        then
+            mpv $est
+            exit
+        fi
+        if [ "$est" = "$1" ]
+        then
+            found=1
+        fi
+    done
+    echo "$1 no se encontró"
+}
+
+if [ "$#" == "0" ]
+then
+    echo "Estaciones de radio:"
+    n=0
+    for est in $estaciones
+    do
+        if [ "$n" = "0" ]
+        then
+            echo $est
+            n=1
+        else
+            n=0
+        fi
+    done
+    exit
+fi
+
+while [ -n "${1}" ]
+do
+    case "$1" in
+        -d|--debug)
+            set -x
+        ;;
+        -e|--error)
+            set -e
+        ;;
+        *)
+            toca $1
+        ;;
+    esac
+    shift
+done
+
