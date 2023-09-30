5b2a075998b816bdc323ca2832fb4f0511ea34bf
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2023-09-30 13:03:56 -0600

    extra: add user miguel

--- extra/add-user-miguel.sh
+++ extra/add-user-miguel.sh
@@ -1,2 +1,29 @@
-useradd --uid 1980 --shell /usr/bin/bash --create-home --comment "Miguel Bernal Marin" --groups cdrom,dip,plugdev,lpadmin,lxd,familia,sambashare  --home-dir /home/home/miguel  miguel
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
+source /usr/lib/os-release
+
+if [ $UID -ne 0 ]
+then
+    echo "Please run with \"root\" or \"sudo $0\""
+    exit 1
+fi
+
+useradd \
+	--uid 1980 \
+	--shell /usr/bin/bash \
+	--create-home \
+	--home-dir /home/miguel \
+	--comment "Miguel Bernal Marin" \
+	miguel
+
+if [ -d /etc/sudoers.d ]
+then
+    echo "miguel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/015_miguel-nopasswd
+fi
+
+#	--groups cdrom,dip,plugdev,lpadmin,lxd,familia,sambashare  \
